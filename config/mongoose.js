db77061250833f380b72fc8f14d1cb36b0fd69fd
premnarayanp <premnarayanpatel223@gmail.com>
2023-11-25 19:24:41 +0530

    db URL from process.env

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect(process.env.MONGO_DB_URL);
-mongoose.connect("mongodb://127.0.0.1/pdf_page_extractor");
+mongoose.connect(process.env.MONGO_DB_URL);
+//mongoose.connect("mongodb://127.0.0.1/elder_helth_care");
 
 const db = mongoose.connection;
 
